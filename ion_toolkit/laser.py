60f6e347fcc73b3175cafe89973d8efad48c7c2a
Gyeonghun Kim <gyeonghun.kim@duke.edu>
2025-06-08 02:25:56 -0400

    Branching ratio

--- ion_toolkit/laser.py
+++ ion_toolkit/laser.py
@@ -12,4 +12,46 @@ class Polarization:
         """
         self.epsilon_0 = epsilon_0
         self.epsilon_1 = epsilon_1
-       
+        self.k_hat = k_hat / np.linalg.norm(k_hat)
+        z_hat = np.array([0, 0, 1])
+        if np.isclose(np.dot(self.k_hat, z_hat), 1):
+            self.epsilon_0_unit_vector = np.array([1, 0, 0])
+            self.epsilon_1_unit_vector = np.array([0, 1, 0])
+        else:
+            self.epsilon_0_unit_vector = z_hat - np.dot(z_hat, self.k_hat) * self.k_hat
+            self.epsilon_0_unit_vector = self.epsilon_0_unit_vector / np.linalg.norm(
+                self.epsilon_0_unit_vector
+            )
+
+        self.epsilon_1_unit_vector = np.cross(self.k_hat, self.epsilon_0_unit_vector)
+        self.epsilon_in_cartesian = (
+            self.epsilon_0 * self.epsilon_0_unit_vector
+            + self.epsilon_1 * self.epsilon_1_unit_vector
+        )
+        self.epsilon_in_spherical_tensor = np.array(
+            [
+                (self.epsilon_in_cartesian[0] - 1j * self.epsilon_in_cartesian[1])
+                / np.sqrt(2),
+                self.epsilon_in_cartesian[2],
+                -(self.epsilon_in_cartesian[0] + 1j * self.epsilon_in_cartesian[1])
+                / np.sqrt(2),
+            ]
+        )  # in order of q = -1, q = 0, q = 1
+
+
+class Laser:
+    def __init__(
+        self,
+        wavelength: float,
+        intensity: float,
+        line_width: float,
+        polarization: Polarization,
+    ):
+        self.wavelength = wavelength
+        self.intensity = intensity
+        self.line_width = line_width
+        self.polarization = polarization
+        self.k_hat = polarization.k_hat
+
+    def get_frequency(self):
+        return Constants.c / self.wavelength
